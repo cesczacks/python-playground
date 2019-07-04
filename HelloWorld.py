5002e0a1c55bb1a8a983c8d1013407bb108d8ba4
qinhong.wang <qinhong.wang@nnlife.co.jp>
2019-07-04 17:55:02 +0900

    1

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,10 +1,22 @@
-import requests
+import sys
+import hashlib
 
-proxies = {
-    'http': 'http://KJ69BG:cescFabregas44@nnlj-kk-prxy-vip.jp.intranet:8080',
-    'https': 'http://proxy.jp.intranet:8080'
-}
-
-r = requests.get('https://www.douban.com/', proxies=proxies)
-print(r.status_code)
-print(r.text)
+file = []
+lines = []
+for i in range(2):
+    # open the files named in the command line
+    file.append(open(sys.argv[1+i], 'r'))
+    # stores the hash value and the line number for each line in file i
+    lines.append({})
+    # assuming you like counting lines starting with 1
+    counter = 1
+    while 1:
+        # assuming default encoding is sufficient to handle the input file
+        line = file[i].readline().encode()
+        if not line: break
+        hashcode = hashlib.sha512(line).hexdigest()
+        lines[i][hashcode] = sys.argv[1+i]+': '+str(counter)
+        counter += 1
+unique0 = lines[0].keys() - lines[1].keys()
+unique1 = lines[1].keys() - lines[0].keys()
+result = [lines[0][x] for x in unique0] + [lines[1][x] for x in unique1]
