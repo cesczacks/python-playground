4a51d4c187d0afe19160371eeb18c0555cbeaf57
qinhong.wang <qinhong.wang@nnlife.co.jp>
2019-07-05 11:41:19 +0900

    1

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,22 +1,9 @@
-import sys
-import hashlib
+import pandas as pd
 
-file = []
-lines = []
-for i in range(2):
-    # open the files named in the command line
-    file.append(open(sys.argv[1+i], 'r'))
-    # stores the hash value and the line number for each line in file i
-    lines.append({})
-    # assuming you like counting lines starting with 1
-    counter = 1
-    while 1:
-        # assuming default encoding is sufficient to handle the input file
-        line = file[i].readline().encode()
-        if not line: break
-        hashcode = hashlib.sha512(line).hexdigest()
-        lines[i][hashcode] = sys.argv[1+i]+': '+str(counter)
-        counter += 1
-unique0 = lines[0].keys() - lines[1].keys()
-unique1 = lines[1].keys() - lines[0].keys()
-result = [lines[0][x] for x in unique0] + [lines[1][x] for x in unique1]
+filePath = r'\\SAJPFAP01001\HPCShare\FDM\FDM SIT\RAFM1\COLI\2018\FC\1118_FC\COLI_FC(11-2018).csv'
+
+reader = pd.read_csv(filePath, usecols=["CRTABLE", "ZRLCRTBL"], iterator=True, chunksize=1000)
+
+df = pd.concat(reader, ignore_index=True)
+print(df)
+
