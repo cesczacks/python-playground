052cf2258b9d9500d89f8dd5a54946493405c7fb
qinhong.wang <qinhong.wang@nnlife.co.jp>
2019-07-11 14:06:17 +0900

    1

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,10 +1,19 @@
 import pandas as pd
 
-csvDir = r"C:\Users\KJ69BG\Documents\FERM Evidence\(After) if201811FC_lapse_new_group_modNCD.csv"
-selectFields = ['chdrnum', 'group', 'lcv_term', 'bcestrm']
-policyNumber = '111'
+beforeCsvPath = r"C:\Users\KJ69BG\Documents\FERM Evidence\(Before) if201811FC_lapse_new_group_modFERM.csv"
+afterCsvPath = r"C:\Users\KJ69BG\Documents\FERM Evidence\(After_II) if201811FC_lapse_new_group_modFERM.csv"
 
-df = pd.read_csv(csvDir, usecols=selectFields)
-result = df.query("chdrnum==%s" %(policyNumber))
+policyNumber= '243'
+selectFields = ['chdrnum', 'group', 'crtable', 'zrlcrtbl','lcv_term', 'bcestrm']
 
-print(result)
+df_before = pd.read_csv(beforeCsvPath, usecols=selectFields, low_memory=False)
+df_after = pd.read_csv(afterCsvPath, usecols=selectFields, low_memory=False)
+
+merge = pd.merge(df_before, df_after, on = 'chdrnum', how = 'left')
+
+before_result = df_before.query("chdrnum==%r" %(policyNumber))
+after_result = df_after.query("chdrnum==%r" %(policyNumber))
+
+print(before_result)
+print('----------------------------------------------------------------------------')
+print(after_result)
