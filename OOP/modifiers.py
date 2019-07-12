d3c18c8326545325dc3a3124a983f27c7f6091f1
qinhong.wang <qinhong.wang@nnlife.co.jp>
2019-07-12 11:28:23 +0900

    1

--- OOP/modifiers.py
+++ OOP/modifiers.py
@@ -28,8 +28,17 @@ print(cesc.get_score())
 cesc.set_score(100)
 print(cesc.get_score())
 
+## Practice
+class Player(object):
+    
+    def __init__(self, name, gender):
+        self.name = name
+        self.__gender = gender
 
+    def get_gender(self):
+        return self.__gender
 
-
+    def set_gender(self, gender):
+        self.__gender = gender
 
 
