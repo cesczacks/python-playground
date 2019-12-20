5eb6010b71797e6943c4a368bbc9626533f9e587
qinhong.wang <qinhong.wang@nnlife.co.jp>
2019-12-20 13:17:14 +0900

    1

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,19 +1,30 @@
-import pandas as pd
+class Student:
+    def __init__(self, name, job=None, time=0.00, time_effective=0.80): 
+        self.name = name
+        self.job = job
+        self.time = time
+        self.time_effective = time_effective
 
-beforeCsvPath = r"C:\Users\KJ69BG\Documents\FERM Evidence\(Before) if201811FC_lapse_new_group_modFERM.csv"
-afterCsvPath = r"C:\Users\KJ69BG\Documents\FERM Evidence\(After_II) if201811FC_lapse_new_group_modFERM.csv"
+    def count_time(self, hour, rate = 0.8):
+        self.time += hour
+        self.time_effective += hour * rate
 
-policyNumber= '243'
-selectFields = ['chdrnum', 'group', 'crtable', 'zrlcrtbl','lcv_term', 'bcestrm']
+# 请你完成子类的定制（包括初始化方法和count_time函数）
+class Programmer(Student):
+    job = 'programmer'
 
-df_before = pd.read_csv(beforeCsvPath, usecols=selectFields, low_memory=False)
-df_after = pd.read_csv(afterCsvPath, usecols=selectFields, low_memory=False)
+    def count_time(self, hour, rate = 1):
+        self.time += hour
+        self.time_effective += hour * rate
 
-merge = pd.merge(df_before, df_after, on = 'chdrnum', how = 'left')
 
-before_result = df_before.query("chdrnum==%r" %(policyNumber))
-after_result = df_after.query("chdrnum==%r" %(policyNumber))
 
-print(before_result)
-print('----------------------------------------------------------------------------')
-print(after_result)
+# 通过两个实例，完成子类和父类的对比（可自行验证）
+student1 = Student('韩梅梅')
+student2 = Programmer('李雷')
+
+student1.count_time(10, 0.8)
+print('%s - %s' %(student1.time, student1.time_effective))
+
+student2.count_time(10, 1)
+print('%s - %s' %(student2.time, student2.time_effective))
---
+++ pandasTest.py
@@ -0,0 +1,3 @@
+fileName = r"\TV_TW_V_Rate.csv";
+
+print(fileName[:len(fileName) - 4]);
